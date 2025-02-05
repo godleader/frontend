1e8ba3922c1c15ccfcc2a203825042a2aa7fb2d1
godleader <kenny9@gmail.com>
2025-02-06 01:11:12 +0800

    ok

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./index.module.css";
 
 import { motion } from "framer-motion";
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
 import App from "./App";
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <>
-        <App />
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-    </>
-  );
-}
 
-const root = createRoot(document.getElementById('root'));
-root.render(<Counter />);
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
